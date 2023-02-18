392209073b8ab483fb5916a2c794caccd12ddf69
Akshay Patil <33000012@heytorus.com>
2023-02-18 15:53:09 +0530

    add function in listing page using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -22,6 +22,7 @@ import {
 } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 // import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Listing() {
@@ -148,13 +149,13 @@ export default function Listing() {
           )}
           {contactLandlord && <Contact userRef={listing.userRef} listing={listing}/>}
         </div>
-        {/* <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2"> */}
-        {/* <MapContainer
-            center={[listing.geolocation.lat, listing.geolocation.lng]}
+        {/* <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+            <MapContainer
+            center={[listing.lat, listing.lng]}
             zoom={13}
             scrollWheelZoom={false}
             style={{ height: "100%", width: "100%" }}
-          >
+            >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -166,8 +167,8 @@ export default function Listing() {
                 {listing.address}
               </Popup>
             </Marker>
-          </MapContainer> */}
-        {/* </div> */}
+            </MapContainer>
+        </div> */}
       </div>
     </main>
   );
